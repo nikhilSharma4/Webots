f64bbbbfbf0fbdc96da558c75f2823854c02d2e9
Nikhil Sharma <nikhilsharma@uwaterloo.ca>
2024-04-28 10:10:34 -0400

    adding webots files

---
+++ controllers/tiago++/build/release/tiago++.d
@@ -0,0 +1,9 @@
+build/release/tiago++.o: tiago++.c \
+  /Applications/Webots.app/Contents/include/controller/c/webots/camera.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/camera_recognition_object.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/types.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/keyboard.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/motor.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/range_finder.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/nodes.h
